c6ce43a28c8b169f199b6ce7650a3f40643f810e
Serkhani <deviper03@gmail.com>
2022-08-15 11:37:10 +0000

    some code refactoring

--- lib/models/about_img.dart
+++ lib/models/about_img.dart
@@ -6,24 +6,44 @@ class AboutImg extends StatelessWidget {
   final Person person;
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: person.name,
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.6,
-        decoration: BoxDecoration(
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.grey,
-              spreadRadius: 15.0,
-              blurRadius: 15.0,
-            )
-          ],
-          borderRadius:
-              const BorderRadius.vertical(bottom: Radius.circular(30.0)),
-          image: DecorationImage(
-              image: NetworkImage(person.image), fit: BoxFit.cover),
+    return Stack(
+      children: [
+        Hero(
+          tag: person.name,
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.6,
+            decoration: BoxDecoration(
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  spreadRadius: 15.0,
+                  blurRadius: 15.0,
+                )
+              ],
+              borderRadius:
+                  const BorderRadius.vertical(bottom: Radius.circular(30.0)),
+              image: DecorationImage(
+                  image: NetworkImage(person.image), fit: BoxFit.cover),
+            ),
+          ),
         ),
-      ),
+        SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.cancel_sharp,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,29 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:socials/Utils/person.dart';
+import 'package:socials/models/about_img.dart';
 
 class Profile extends StatelessWidget {
   const Profile({Key? key, required this.person}) : super(key: key);
   final Person person;
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Hero(
-          tag: person.name,
-          child: Card(
-            semanticContainer: true,
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            elevation: 5,
-            margin: const EdgeInsets.all(10),
-            child: Image.network(
-              person.image,
-              width: MediaQuery.of(context).size.width,
-              fit: BoxFit.fill,
-            ),
-          ),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AboutImg(person: person),
+          ],
         ),
       ),
     );
