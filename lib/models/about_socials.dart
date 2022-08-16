088cefe3d686ac0eabe8aad9ab155338c9b1c509
Serkhani <deviper03@gmail.com>
2022-08-16 10:00:42 +0000

    added border to socials column

--- lib/models/about_socials.dart
+++ lib/models/about_socials.dart
@@ -1,46 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:socials/Utils/social.dart';
+import 'package:socials/models/add_social_dialog.dart';
 
 class AboutSocials extends StatelessWidget {
   const AboutSocials({Key? key, required this.socials}) : super(key: key);
   final List<Social> socials;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Socials",
-                style: Theme.of(context).primaryTextTheme.titleMedium,
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(12.0)),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Socials",
+                    style: Theme.of(context).primaryTextTheme.titleMedium,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return const AddSocialDialog();
+                        },
+                      );
+                    },
+                    splashRadius: 12.0,
+                    icon: Icon(
+                      Icons.add_circle_sharp,
+                      color:
+                          Theme.of(context).primaryTextTheme.titleMedium?.color,
+                    ),
+                  ),
+                ],
               ),
-              IconButton(
-                onPressed: () {},
-                splashRadius: 12.0,
-                icon: Icon(
-                  Icons.add_circle_sharp,
-                  color: Theme.of(context).primaryTextTheme.titleMedium?.color,
-                ),
+            ),
+            SizedBox(
+              height: 300.0,
+              child: ListView.builder(
+                itemCount: socials.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0)),
+                      child: ListTile(title: Text(socials[index].platform)));
+                },
               ),
-            ],
-          ),
+            )
+          ],
         ),
-        SizedBox(
-          height: 200.0,
-          child: ListView.builder(
-            itemCount: socials.length,
-            itemBuilder: (context, index) {
-              return Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0)),
-                  child: ListTile(title: Text(socials[index].platform)));
-            },
-          ),
-        )
-      ],
+      ),
     );
   }
 }
